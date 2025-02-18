dd2ce1e86221e17162ab43565523c3d6ba4d31cc
Kassy Raymond <kassyraymond2@gmail.com>
2025-02-18 14:21:33 -0500

    Modified tables to reflect revised schema.

--- sql_scripts/create_tables.sql
+++ sql_scripts/create_tables.sql
@@ -8,12 +8,6 @@ CREATE TABLE users (
     user_role VARCHAR(50) NOT NULL
 )
 
-CREATE TABLE models (
-    model_id SERIAL PRIMARY KEY,
-    model_name VARCHAR(100) NOT NULL,
-    model_version VARCHAR(20) NOT NULL
-)
-
 CREATE TABLE user_dashboards (
     dashboard_id SERIAL PRIMARY KEY,
     user_id SERIAL REFERENCES users(user_id) NOT NULL,
@@ -26,12 +20,12 @@ CREATE TABLE user_dashboards (
 
 CREATE TABLE user_models (
     user_id SERIAL REFERENCES users(user_id) NOT NULL,
-    model_id SERIAL REFERENCES models(model_id) NOT NULL,
     user_model_id SERIAL PRIMARY KEY,
     user_model_name VARCHAR(100) NOT NULL,
     user_model_status VARCHAR(100) NOT NULL,
     user_model_summary_s3_url TEXT NOT NULL,
     user_model_full_s3_url TEXT NOT NULL,
+    user_model_yaml_s3_url TEXT NOT NULL,
     date_created TIMESTAMP NOT NULL,
     date_completed TIMESTAMP NOT NULL
 )
