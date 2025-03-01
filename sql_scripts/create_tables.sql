5308a519765a9f2fdaf5c5a7b996a381d9534df7
Kassy Raymond <kassyraymond2@gmail.com>
2025-03-01 07:00:57 -0500

    Updates to create tables

--- sql_scripts/create_tables.sql
+++ sql_scripts/create_tables.sql
@@ -8,24 +8,8 @@ CREATE TABLE users (
     user_role VARCHAR(50) NOT NULL
 )
 
-CREATE TABLE user_dashboards (
-    dashboard_id SERIAL PRIMARY KEY,
-    user_id SERIAL REFERENCES users(user_id) NOT NULL,
-    user_model_id SERIAL REFERENCES user_models(user_model_id) NOT NULL,
-    date_created TIMESTAMP NOT NULL,
-    date_updated TIMESTAMP NOT NULL,
-    dashboard_s3_url TEXT NOT NULL,
-    dashboard_name VARCHAR(100) NOT NULL
-)
-
-CREATE TABLE user_models (
-    user_id SERIAL REFERENCES users(user_id) NOT NULL,
-    user_model_id SERIAL PRIMARY KEY,
-    user_model_name VARCHAR(100) NOT NULL,
-    user_model_status VARCHAR(100) NOT NULL,
-    user_model_summary_s3_url TEXT NOT NULL,
-    user_model_full_s3_url TEXT NOT NULL,
-    user_model_yaml_s3_url TEXT NOT NULL,
-    date_created TIMESTAMP NOT NULL,
-    date_completed TIMESTAMP NOT NULL
+CREATE models (
+    model_id SERIAL PRIMARY KEY,
+    model_name VARCHAR(50) NOT NULL,
+    model_version 
 )
