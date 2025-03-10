ca6cec41b7f39d4dc7e778556c559e213fac51a8
Kassy Raymond <kassyraymond2@gmail.com>
2025-03-10 12:21:05 -0400

    Code to create a presigned url for uploading data into an s3 bucket. This is an important function to upload modelling templates after the user fills them out. Not sure how this will work in R Shiny but maybe reticulate is an option #8.

---
+++ scripts/presigned_URL.py
@@ -0,0 +1,46 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import requests
+
+
+def create_presigned_post(bucket_name, object_name,
+                          fields=None, conditions=None, expiration=3600):
+    """Generate a presigned URL S3 POST request to upload a file
+
+    :param bucket_name: string
+    :param object_name: string
+    :param fields: Dictionary of prefilled form fields
+    :param conditions: List of conditions to include in the policy
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Dictionary with the following keys:
+        url: URL to post to
+        fields: Dictionary of form fields and values to submit with the POST
+    :return: None if error.
+    """
+
+    # From: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-presigned-urls.html
+
+    # Generate a presigned S3 POST URL
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_post(bucket_name,
+                                                     object_name,
+                                                     Fields=fields,
+                                                     Conditions=conditions,
+                                                     ExpiresIn=expiration)
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL and required fields
+    return response
+
+def use_presigned_url(response, object_name, file):
+
+    # Demonstrate how another Python program can use the presigned URL to upload a file
+    with open(object_name, 'rb') as f:
+        files = {'file': (object_name, f)}
+        http_response = requests.post(response['url'], data=response['fields'], files=file)
+    # If successful, returns HTTP status code 204
+    logging.info(f'File upload HTTP status code: {http_response.status_code}')
