5b575b418f2776b26186610d8ca7caa53267b6b3
Kassy Raymond <kassyraymond2@gmail.com>
2025-03-10 10:18:15 -0400

    Added function to download template from S3 #6

---
+++ r_scripts/s3_public_utils.R
@@ -0,0 +1,28 @@
+library("RCurl")
+
+# The templates are in an S3 bucket that is public meaning that you shouldn't need security information to 
+# access the data. This means that for public utils we can just use the RCurl package
+
+download_template <- function(species = c("cattle","poultry","small_ruminants")){
+  
+  species <- match.arg(species)
+  
+  template_urls <- list(
+    cattle = "https://gbads-modelling.s3.ca-central-1.amazonaws.com/20250310_DPM_template_cattle.xlsx",
+    poultry = "https://gbads-modelling.s3.ca-central-1.amazonaws.com/20250310_DPM_template_poultry.xlsx",
+    small_ruminants = "https://gbads-modelling.s3.ca-central-1.amazonaws.com/20250310_DPM_template_smallruminants.xlsx"
+  )
+  
+  dest_files <- list(
+    cattle = "DPM_template_cattle.xlsx",
+    poultry = "DPM_template_poultry.xlsx",
+    small_ruminants = "DPM_template_small_ruminants.xlsx"
+  )
+  
+  download.file(url = template_urls[[species]],
+                destfile = dest_files[[species]],
+                method="curl")
+  
+}
+
+
