f9ac21e1c98e55540e437546685c0d125e4a56b0
Kassy Raymond <kassyraymond2@gmail.com>
2025-03-17 10:07:10 -0400

    Adding test to check if instructions and data dictionary are also downloaded properly #10

--- r_scripts_test/test_s3_public_utils.R
+++ r_scripts_test/test_s3_public_utils.R
@@ -8,16 +8,16 @@ test_that(desc = "Download species Dynamic Population Model (DPM) template from 
   dest_files <- list(
     cattle = "DPM_template_cattle.xlsx",
     poultry = "DPM_template_poultry.xlsx",
-    small_ruminants = "DPM_template_small_ruminants.xlsx"
+    small_ruminants = "DPM_template_small_ruminants.xlsx",
+    instructions_dest_file = "instructions.pdf",
+    data_dict_dest_file = "data_dictionary.xlsx"
   )
   
   # Check to see if dest_files already exist in the testdir. If they do we want to remove them so that the test can test properly.
-  
-  
-  for (i in species){
-    
-    if (file.exists(file = dest_files[[i]]) == TRUE) {
-      stop("Species template already exists. Please remove it from the testing directory.")
+  for (i in dest_files){
+
+    if (file.exists(file = i)) {
+      stop(sprintf("%s already exists. Please remove it from the testing directory.", i))
     }
     
   }
@@ -26,8 +26,17 @@ test_that(desc = "Download species Dynamic Population Model (DPM) template from 
     
     download_template(i)
     
-    expect_that(object = file.exists(file = dest_files[[i]]), condition = equals(TRUE));
+    # I feel like an apply function family situation would make the code below better, if there is time. 
+    expect_that(file.exists(dest_files[[i]]), equals(TRUE))
+    expect_that(file.exists(dest_files$instructions_dest_file), equals(TRUE))
+    expect_that(file.exists(dest_files$data_dict_dest_file), equals(TRUE))
+    
+    file.remove(c(dest_files$data_dict_dest_file, dest_files$instructions_dest_file))
       
-    }
+  }
+  
+  for (i in species){
+    file.remove(dest_files[[i]])
+  }
     
   })
