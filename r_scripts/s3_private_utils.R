77319ffff6a53bbec175a29044eed86427831fe3
Kassy Raymond <kassyraymond2@gmail.com>
2025-03-24 12:12:40 -0400

    Refactored and cleaned the code so that it is tidy and in functions #8.

--- r_scripts/s3_private_utils.R
+++ r_scripts/s3_private_utils.R
@@ -1,34 +1,50 @@
 library("paws")
 
-s3 <- paws::s3()
+# Note that to make this work you need to first ensure that you set your sys.env 
+# credentials using the secret keys in aws 
 
-file = "/Users/kassyraymond/Documents/GBADs-ARIF-Grant/r_scripts/test_upload.txt"
-
-bucket_name = "gbads-modelling-private"
-
-s3$list_objects(Bucket = "gbads-modelling-private")
-
-# Create client
-svc <- s3(
-  config = list(
-    credentials = list(
-      creds = list(
-        access_key_id = "your AWS access key",
-        secret_access_key = "your AWS secret key"
+create_client <- function(){
+  
+  s3 <- paws::s3()
+  
+  svc <- s3(
+    config = list(
+      credentials = list(
+        creds = list(
+          access_key_id = "your AWS access key",
+          secret_access_key = "your AWS secret key"
+        )
       )
     )
   )
-)
+  
+  return(svc)
+  
+}
 
-url <- svc$generate_presigned_url(
-  client_method = "get_object",
-  params = list(Bucket = bucket_name, Key = "test_upload.txt"),
-  expires_in = 3600
-)
+get_object_url <- function(bucket_name, file_name){ 
+  
+  # The key here is the name of the file that you want to give the user access to.
 
-url <- svc$generate_presigned_url(
-  client_method = "put_object",
-  params=list(Bucket = bucket_name, 
-              Key = "test_file.txt"), 
-  expires_in=3600,
-  http_method = NULL)
+  url <- svc$generate_presigned_url(
+    client_method = "get_object",
+    params = list(Bucket = bucket_name, Key = file_name),
+    expires_in = 3600
+  )
+  
+  return(url)
+  
+}
+
+put_object_url <- function(bucket_name, file_name){
+  
+  # The key has to be the name of the file that you want to be able to write to.
+
+  url <- svc$generate_presigned_url(
+    client_method = "put_object",
+    params=list(Bucket = bucket_name, 
+                Key = file_name), 
+    expires_in=3600,
+    http_method = NULL)
+}
+
