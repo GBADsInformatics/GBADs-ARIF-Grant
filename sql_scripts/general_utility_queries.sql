26c67e37be040f4f8b0d68e2296f2b40890f3557
WilliamFitzjohn <william_fitzjohn@icloud.com>
2025-11-09 14:03:19 -0500

    deploying in canada

--- sql_scripts/general_utility_queries.sql
+++
@@ -1,9 +0,0 @@
-/*
-View all tables in the db 
-*/
-SELECT * FROM pg_catalog.pg_tables WHERE schemaname='public';
-
-/*
-Drop a table
-*/
-DROP TABLE user_dashboard
--- sql_scripts/insert_user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (user_firstname, user_lastname, user_email, user_country, user_language, user_role)
-VALUES ('Kassy', 'Raymond', 'kassyraymond2@gmail.com', 'Canada', 'English', 'Student');
